ab32c3e7aeb51ba35eb5bc73c4a237394244514b
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-13 09:22:26 +0200

    add __str__ methods on models

--- m2istrpoll/election/models.py
+++ m2istrpoll/election/models.py
@@ -5,6 +5,9 @@ class Election(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
 
+    def __str__(self):
+        return self.title
+
 
 class Candidate(models.Model):
     election = models.ForeignKey(
@@ -13,3 +16,6 @@ class Candidate(models.Model):
     )
     name = models.CharField(max_length=250)
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
