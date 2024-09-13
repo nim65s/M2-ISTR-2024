c6efba70ecfa4a0e30decfb988a877b7ab9a0dc3
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-13 09:10:48 +0200

    modèles des élections

--- m2istrpoll/election/models.py
+++ m2istrpoll/election/models.py
@@ -1,1 +1,12 @@
-# Create your models here.
+from django import models
+
+
+class Election(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateField()
+
+
+class Candidate(models.Model):
+    election = models.ForeignKey(Election)
+    name = models.CharField(max_length=250)
+    score = models.IntegerField(default=0)
